03f5739b50eb4ccc7ce0fc82258f3c3501072df5
sayedbasir110 <basir.sayedali@gmail.com>
2024-07-30 16:40:06 -0700

    cucumber

---
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -0,0 +1,22 @@
+package tek.bdd.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseSetup {
+    // encapsulating driver instance
+    private static WebDriver driver;
+    public void setupBrowser () {
+        driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
+    public void quitBrowser() {
+        // null check before quit
+        if (driver != null) driver.quit();
+    }
+
+}
