8f3e3242d85ddd6f1971d261e799922ce6ce45e0
sayedbasir110 <basir.sayedali@gmail.com>
2024-07-30 17:14:14 -0700

    cucumber

--- src/test/java/tek/bdd/utility/SeleniumUtility.java
+++ src/test/java/tek/bdd/utility/SeleniumUtility.java
@@ -2,6 +2,7 @@ package tek.bdd.utility;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import tek.bdd.base.BaseSetup;
@@ -13,12 +14,20 @@ public class SeleniumUtility extends BaseSetup {
     private WebDriverWait getWait() {
         return  new WebDriverWait(getDriver(), Duration.ofSeconds(20));
     }
+    // method to wait for visibility of an element and return it
+    private WebElement waitForVisibility (By locator){
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
     // method to click on a given locator
     public void clickOnElement (By locator){
         getWait().until(ExpectedConditions.elementToBeClickable(locator)).click();
     }
     // method to send string to a given locator
     public void sendKeysToElement (By locator, String value) {
-        getWait().until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(value);
+        waitForVisibility(locator).sendKeys();
+    }
+    // method to return text of an element
+    public String getElementText (By locator){
+        return waitForVisibility(locator).getText();
     }
 }
